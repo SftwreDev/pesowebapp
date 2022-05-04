67abc2d84d717b285d3fc8bdcc43d971ca2b0bb9
Gian Carlo Llanes Garcia <49069702+SftwreDev@users.noreply.github.com>
2022-05-04 12:42:04 +0800

    Update email

--- pesowebapp/settings.py
+++ pesowebapp/settings.py
@@ -153,5 +153,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "pesotest00@gmail.com"
-EMAIL_HOST_PASSWORD = "pesomalvar"
+EMAIL_HOST_USER = "pesomalvar@gmail.com"
+EMAIL_HOST_PASSWORD = "kvhbCeMT123"
